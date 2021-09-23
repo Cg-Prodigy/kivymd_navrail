5077b21750379c06922cf34e83f32ab9ed53b2e7
Cg-Prodigy <brightmarshall2@gmail.com>
2021-09-23 15:14:57 +0300

    KivyMD NavigationRail

--- app/main_ui.py
+++ app/main_ui.py
@@ -1,5 +1,18 @@
-from kivymd.uix.floatlayout import MDFloatLayout
+from kivy.clock import Clock
+from kivy.uix.screenmanager import ScreenManager
+from kivymd.uix.screen import MDScreen
+from kivymd.uix.list import OneLineListItem
 
 
-class FloatBuild(MDFloatLayout):
+class EntryPoint(ScreenManager):
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        for i in range(20):
+            self.opt = OneLineListItem(
+                text=f'Text Number {i}'
+            )
+
+
+class IntroScreen(MDScreen):
     pass
--- main.py
+++ main.py
@@ -3,20 +3,23 @@ from kivy.factory import Factory
 from kaki.app import App
 from kivymd.app import MDApp
 
-Window.size=(360,640)
-class HotReload(App,MDApp):
-    CLASSES={
-        'FloatBuild':'app.main_ui'
+#Window.size = (360, 640)
+
+
+class HotReload(App, MDApp):
+    CLASSES = {
+        'EntryPoint': 'app.main_ui'
     }
-    KV_FILES=[
+    KV_FILES = [
         'app/kivy_lang.kv'
     ]
-    AUTORELOADER_PATHS=[
-        ('.',{'recursive':True})
+    AUTORELOADER_PATHS = [
+        ('.', {'recursive': True})
     ]
 
-    def build_app(self, first):
-        return Factory.FloatBuild()
+    def build_app(self):
+        return Factory.EntryPoint()
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     HotReload().run()
