beab6e59a141851babae935abe4154b7d8edd081
Cg-Prodigy <brightmarshall2@gmail.com>
2021-09-25 13:57:26 +0300

    Navigation Rail take two. Still alot to take in

--- app/main_ui.py
+++ app/main_ui.py
@@ -1,17 +1,30 @@
 from kivy.clock import Clock
+from kivy.properties import ObjectProperty
+from kivy.metrics import dp
 from kivy.uix.screenmanager import ScreenManager
 from kivymd.uix.screen import MDScreen
 from kivymd.uix.list import OneLineListItem
+from kivy.utils import get_color_from_hex
 
 
 class EntryPoint(ScreenManager):
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        for i in range(20):
-            self.opt = OneLineListItem(
-                text=f'Text Number {i}'
-            )
+
+    def open_rail(self):
+        rail = self.ids.nav_rail
+        if rail.rail_state == 'open':
+            rail.rail_state = 'close'
+        else:
+            rail.rail_state = 'open'
+
+    def set_item_color(self, instance):
+        instance.color_active = get_color_from_hex('#ff4500')
+        instance.color_normal = [1, 1, 1]
+
+    def set_colors(self, instance):
+        print(instance.children)
 
 
 class IntroScreen(MDScreen):
